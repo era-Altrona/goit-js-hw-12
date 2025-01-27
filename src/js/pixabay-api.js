f66aefbdaa46039d63505bbed640c9162df80a15
Alina Komerysta <alinakomerista3@gmail.com>
2025-01-27 18:12:01 +0100

    commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,7 +10,6 @@ export function serviceImages(question) {
     orientation: 'horizontal',
     safesearch: true,
     per_page: 15,
-    page: page,
   });
 
   return fetch(`${BASE_URL}?${params}`).then(response => {
